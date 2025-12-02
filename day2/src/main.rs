44982cbb2124b426aaad15e698ccc659894b38ae
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-02 14:09:10 +0100

    Day(2): Solved part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -37,6 +37,49 @@ fn is_text_half_repeated(text: &str) -> bool {
     &text[..half] == &text[half..]
 }
 
+/// Examples:
+///     "123123123" -> true because "123" == "123" == "123"
+///     "111111"    -> true because // Composed of "1"s
+fn is_text_made_of_sequence(text: &str) -> bool {
+    let l = text.len();
+
+    // The algorithm detects repeated sequences by comparing all blocks of text
+    // side by side.
+    // At the beginning, the text is split into a few big blocks, and as we go
+    // it gets divided into more and smaller blocks until making each block
+    // contain one character
+    let mut block_num: usize = 1;
+
+    while block_num < l {
+        // Slice text into smaller blocks
+        loop {
+            block_num += 1;
+            if l % block_num == 0 {
+                break;
+            }
+        }
+
+        // Count how many block are equal
+        let block_size: usize = l / block_num;
+        let mut offset: usize = block_size;
+        let mut record = 0;
+        for _ in 1..block_num {
+            let (a, b) = (&text[..block_size], &text[offset..offset + block_size]);
+            if a == b {
+                record += 1;
+            };
+            offset += block_size;
+        }
+
+        // If all blocks are equal, then we've found the sequence
+        if record == block_num - 1 {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 /// The mistery is the sum of the invalid ids
 /// Invalidator is a function that returns true when invalid
 fn decode_mistery<T>(ranges: &Vec<Range<Int>>, invalidator: T) -> Int
@@ -58,4 +101,7 @@ fn main() {
     // Return solutions
     let password_1 = decode_mistery(&ranges, is_text_half_repeated);
     println!("Part 1: {password_1}");
+
+    let password_2 = decode_mistery(&ranges, is_text_made_of_sequence);
+    println!("Part 2: {password_2}");
 }
