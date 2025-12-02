607dbd0a42d18ede5502259468c07ff3d52146cf
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-02 12:19:01 +0100

    Day(2): Parse input

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,28 @@
+use std::ops::Range;
+use utils::quick_read;
+
+fn parse_text_into_ranges(text: &str) -> Vec<Range<u64>> {
+    text.split(',')
+        .map(|s| {
+            s.split_once('-')
+                .and_then(|(l, r)| if r.contains('-') { None } else { Some((l, r)) })
+                .map_or_else(
+                    || panic!("{s} range has an invalid format"),
+                    |(l, r)| {
+                        let parse = |s: &str| s.trim().parse::<u64>().unwrap();
+                        Range::<u64> {
+                            start: parse(l),
+                            end: parse(r),
+                        }
+                    },
+                )
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Parse input
+    let text = quick_read!("input.txt");
+    let ranges = parse_text_into_ranges(&text);
+    println!("{ranges:#?}")
 }
