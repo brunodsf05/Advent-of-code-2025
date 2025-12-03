94e4ff8e4d59b5a1e8d3963d07e4b174a944bf35
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-03 09:15:08 +0100

    Day(3): Created Bank type and prepared parser function

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,13 @@
 use utils::quick_read;
 
+type Bank = Vec<i32>;
+
+fn parse_input_into_banks(text: &str) -> Vec<Bank> {
+    Vec::new()
+}
+
 fn main() {
     // Parse input
     let text = quick_read!("input.txt");
+    let banks = parse_input_into_banks(&text);
 }
