05188ddcbc07d1b37398a99752aebaa8c363bd56
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-04 15:01:17 +0100

    Day(4): Initialized parser

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,14 @@
 use utils::quick_read;
 
+type Cell = bool;
+type Grid = Vec<Vec<Cell>>;
+
+fn parse_input_into_grid(text: &str) -> Grid {
+    Vec::new()
+}
+
 fn main() {
     // Parse input
     let text = quick_read!("input.txt");
+    let grid = parse_input_into_grid(&text);
 }
