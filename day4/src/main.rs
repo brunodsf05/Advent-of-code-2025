89123d8d217ea6d28f80b4491cd4dfe6ff52d367
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-04 17:09:14 +0100

    Day(4): Completed part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,7 @@
 mod shared;
 
 use shared::*;
+use std::mem::swap;
 use utils::quick_read;
 
 fn parse_input_into_grid(text: &str) -> Grid {
@@ -36,6 +37,47 @@ fn decode_mistery_1(grid: &Grid) -> i32 {
     forkliftable
 }
 
+fn decode_mistery_2(grid: &Grid) -> i32 {
+    let rows_range = 0..grid.len() as i32;
+    let mut forkliftable = 1;
+    let mut removed = 0;
+
+    let (mut grid_read, mut grid_write) = (grid.clone(), grid.clone());
+
+    while forkliftable > 0 {
+        forkliftable = 0;
+
+        // We use two buffers to make sure each roll is
+        // deleted after iterating trough all the lists and not after
+        // each cell read.
+        //
+        // This avoids a bug where removing a roll immediately would let the
+        // algorithm see that removal while still scanning the current pass,
+        // causing cascaded deletions that should only happen on the next pass.
+
+        for (y, row) in (&grid_read).iter().enumerate() {
+            let cols_range = 0..row.len() as i32;
+
+            for (x, cell) in row.iter().enumerate() {
+                let adjacent_rolls =
+                    count_adjacents_cell(&grid_read, x, y, &cols_range, &rows_range);
+
+                if *cell == true && adjacent_rolls < VALID_TO_FORKLIFT {
+                    forkliftable += 1;
+                    grid_write[y][x] = false;
+                }
+            }
+        }
+
+        grid_read = grid_write;
+        grid_write = grid_read.clone();
+
+        removed += forkliftable;
+    }
+
+    removed
+}
+
 fn main() {
     // Parse input
     let text = quick_read!("input.txt");
@@ -44,4 +86,7 @@ fn main() {
     // Return solutions
     let password_1 = decode_mistery_1(&grid);
     println!("Part 1: {password_1}");
+
+    let password_2 = decode_mistery_2(&grid);
+    println!("Part 2: {password_2}");
 }
