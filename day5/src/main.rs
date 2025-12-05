57c56649d73c59513a22e61fcd74307902785e39
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-05 11:14:14 +0100

    Day(5): Initialized

---
+++ day5/src/main.rs
@@ -0,0 +1,13 @@
+use utils::quick_read;
+
+fn main() {
+    // Parse input
+    let text = quick_read!("input.txt");
+
+    // Return solutions
+    let password_1 = 0;
+    println!("Part 1: {password_1}");
+
+    let password_2 = 0;
+    println!("Part 2: {password_2}");
+}
