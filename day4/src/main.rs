6e9fb2c25f4bafc503915ed8a3d5f688b771127c
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-05 11:13:04 +0100

    Day(4): Improved part 2 by not copying the Vector in each iteration

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,6 @@
 mod shared;
 
 use shared::*;
-use std::mem::swap;
 use utils::quick_read;
 
 fn parse_input_into_grid(text: &str) -> Grid {
@@ -37,40 +36,39 @@ fn decode_mistery_1(grid: &Grid) -> i32 {
     forkliftable
 }
 
-fn decode_mistery_2(grid: &Grid) -> i32 {
+fn decode_mistery_2(grid: &mut Grid) -> i32 {
     let rows_range = 0..grid.len() as i32;
     let mut forkliftable = 1;
     let mut removed = 0;
 
-    let (mut grid_read, mut grid_write) = (grid.clone(), grid.clone());
-
     while forkliftable > 0 {
+        let mut forklift_positions: Vec<(usize, usize)> = Vec::new();
         forkliftable = 0;
 
-        // We use two buffers to make sure each roll is
-        // deleted after iterating trough all the lists and not after
-        // each cell read.
+        // Instead of directly writing each forklift in each iteration, we
+        // first store each forklift position and then at the end we forklift
+        // all forkliftable rolls.
         //
         // This avoids a bug where removing a roll immediately would let the
         // algorithm see that removal while still scanning the current pass,
         // causing cascaded deletions that should only happen on the next pass.
 
-        for (y, row) in (&grid_read).iter().enumerate() {
+        for (y, row) in (&grid).iter().enumerate() {
             let cols_range = 0..row.len() as i32;
 
             for (x, cell) in row.iter().enumerate() {
-                let adjacent_rolls =
-                    count_adjacents_cell(&grid_read, x, y, &cols_range, &rows_range);
+                let adjacent_rolls = count_adjacents_cell(&grid, x, y, &cols_range, &rows_range);
 
                 if *cell == true && adjacent_rolls < VALID_TO_FORKLIFT {
                     forkliftable += 1;
-                    grid_write[y][x] = false;
+                    forklift_positions.push((x, y));
                 }
             }
         }
 
-        grid_read = grid_write;
-        grid_write = grid_read.clone();
+        for (x, y) in forklift_positions.iter() {
+            grid[*y][*x] = false;
+        }
 
         removed += forkliftable;
     }
@@ -81,12 +79,12 @@ fn decode_mistery_2(grid: &Grid) -> i32 {
 fn main() {
     // Parse input
     let text = quick_read!("input.txt");
-    let grid = parse_input_into_grid(&text);
+    let mut grid = parse_input_into_grid(&text);
 
     // Return solutions
     let password_1 = decode_mistery_1(&grid);
     println!("Part 1: {password_1}");
 
-    let password_2 = decode_mistery_2(&grid);
+    let password_2 = decode_mistery_2(&mut grid);
     println!("Part 2: {password_2}");
 }
