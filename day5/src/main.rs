4746af76371f098ba8928c09b2a15a77be7840dc
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-05 11:33:28 +0100

    Day(5): Parsed input

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,12 +4,41 @@ use utils::quick_read;
 type Input = (Vec<Range<usize>>, Vec<usize>);
 
 fn parse_input(text: &str) -> Input {
-    (Vec::new(), Vec::new())
+    let mut ranges: Vec<Range<usize>> = Vec::new();
+    let mut numbers: Vec<usize> = Vec::new();
+
+    for line in text.lines() {
+        let parts: Vec<&str> = line.split('-').collect();
+
+        match parts.len() {
+            1 => {
+                if let Ok(number) = parts[0].parse::<usize>() {
+                    numbers.push(number)
+                }
+            }
+            2 => {
+                let (l, r) = (parts[0].parse::<usize>(), parts[1].parse::<usize>());
+                if l.is_ok() && r.is_ok() {
+                    let (l, r) = (l.unwrap(), r.unwrap());
+                    ranges.push(Range {
+                        start: l,
+                        end: r + 1, // Makes it inclusive
+                    })
+                }
+            }
+            _ => (),
+        };
+    }
+
+    return (ranges, numbers);
 }
 
 fn main() {
     // Parse input
     let text = quick_read!("input.txt");
+    let input = parse_input(&text);
+    let (a, b) = input;
+    println!("{a:?} {b:?}");
 
     // Return solutions
     let password_1 = 0;
