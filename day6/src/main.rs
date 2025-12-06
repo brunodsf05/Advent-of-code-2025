15cf57d74dcd33eb278246230db4707cce7d536f
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-06 22:12:15 +0100

    Day(6): Part 1 solved

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,12 +1,39 @@
+use std::collections::HashMap;
+
 use utils::quick_read;
+
+fn decode_mistery_1(text: &str) -> i64 {
+    let mut sum = 0;
+    let mut number_columns: HashMap<usize, Vec<i64>> = HashMap::new();
+
+    // "111 222 333\n444 555 666" -> ["111 222 333", "444 555 666"]
+    for line in text.lines() {
+        // ["111 222 333"] -> ["111", "222", "333"]
+        for (x, word) in line.split_whitespace().enumerate() {
+            let column = number_columns.entry(x).or_insert(vec![]);
+
+            sum += match word.trim() {
+                "+" => column.iter().sum::<i64>(),
+                "*" => column.iter().product::<i64>(),
+                other => {
+                    if let Ok(number) = other.parse::<i64>() {
+                        column.push(number);
+                    }
+                    0i64
+                }
+            };
+        }
+    }
+
+    return sum;
+}
 
 fn main() {
     // Parse input
     let text = quick_read!("input.txt");
-    let input = "";
 
     // Return solutions
-    let password_1 = 0;
+    let password_1 = decode_mistery_1(&text);
     println!("Part 1: {password_1}");
 
     let password_2 = 0;
