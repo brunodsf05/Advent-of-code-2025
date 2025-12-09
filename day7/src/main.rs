b93b8b600da605220d6c9e19f10f047ff768e23f
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-09 12:08:13 +0100

    Day(7): Part 1 solved

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,6 @@
 use utils::{grid::*, quick_read};
 
+#[derive(Clone, PartialEq)]
 enum Cell {
     Laser,
     Empty,
@@ -27,8 +28,38 @@ fn parse_input_into_grid(text: &str) -> Grid<Cell> {
 }
 
 fn decode_mistery_1(grid: &Grid<Cell>) -> usize {
+    let mut splitters = 0;
+
+    // Prepare iteration with RW grid
+    let (rx, ry) = (0..(grid[0].len() as i32), 0..(grid.len() as i32));
     let mut grid = grid.clone();
-    0
+
+    // Iterate trough every grid's coordinate.
+    // Because the reads occur at the top row (y-1) and writes at the current
+    // (y), we don't need two grids.
+    for y in 1..grid.len() {
+        for x in 0..grid[y].len() {
+            let cell = grid[y][x].clone();
+
+            if get_adjacent_cell(&grid, x, y, 0, -1, &rx, &ry) != Cell::Laser {
+                continue;
+            };
+
+            match &cell {
+                Cell::Laser => (),
+                Cell::Empty => {
+                    grid[y][x] = Cell::Laser;
+                }
+                Cell::Split => {
+                    splitters += 1;
+                    grid[y][x - 1] = Cell::Laser;
+                    grid[y][x + 1] = Cell::Laser;
+                }
+            };
+        }
+    }
+
+    return splitters;
 }
 
 fn decode_mistery_2(grid: &Grid<Cell>) -> usize {
