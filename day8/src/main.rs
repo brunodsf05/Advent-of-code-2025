8669a755cd4736544db408a2552e1680f1a038c8
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-10 17:46:58 +0100

    Day(8): Parse points into vectors which store two points indeces and the squared distance

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,17 +1,44 @@
 use std::fmt;
 use utils::quick_read;
 
+// --- Data Structures ---
 struct Point {
     x: i64,
     y: i64,
     z: i64,
 }
 
+/// Represents a connection between two points.
+/// Stores indeces of a list of points.
+struct Edge(usize, usize);
+
+/// Stores an `Edge` with the squared length between two points.
+struct Vector {
+    e: Edge,
+    l: i64,
+}
+
+// --- Formatting ---
+
 impl fmt::Display for Point {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "X={},Y={},Z={}", self.x, self.y, self.z)
     }
 }
+
+impl fmt::Display for Edge {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "L{}-R{}", self.0, self.1)
+    }
+}
+
+impl fmt::Display for Vector {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}={}", self.e, self.l)
+    }
+}
+
+// --- Data processing ---
 
 fn parse_input_into_points(text: &str) -> Vec<Point> {
     text.lines()
@@ -26,6 +53,30 @@ fn parse_input_into_points(text: &str) -> Vec<Point> {
         .collect()
 }
 
+fn generate_vectors(points: &Vec<Point>) -> Vec<Vector> {
+    let mut vectors: Vec<Vector> = vec![];
+
+    for li in 0..points.len() - 1 {
+        let lp = &points[li];
+        for ri in (li + 1)..points.len() {
+            let rp = &points[ri];
+            let squared_distanced =
+                (lp.x - rp.x).pow(2) + (lp.y - rp.y).pow(2) + (lp.z - rp.z).pow(2);
+
+            vectors.push(Vector {
+                e: Edge(li, ri),
+                l: squared_distanced,
+            });
+        }
+    }
+
+    vectors.sort_by_key(|v| v.l);
+
+    return vectors;
+}
+
+// --- Solutions ---
+
 fn decode_mistery_1(points: &Vec<Point>) -> usize {
     0
 }
@@ -38,7 +89,8 @@ fn main() {
     // Parse input
     let text = quick_read!("input.txt");
     let points = parse_input_into_points(&text);
-    points.iter().for_each(|p| println!("{p}"));
+    let vectors = generate_vectors(&points);
+    vectors.iter().for_each(|v| println!("{v}"));
 
     // Return solutions
     let password_1 = decode_mistery_1(&points);
