24726c8fc14a0b48d8e9a60277cc78c75ac95436
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-10 17:15:26 +0100

    Day(8): Parse input

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,21 +1,49 @@
+use std::fmt;
 use utils::quick_read;
 
-fn decode_mistery_1(text: &str) -> usize {
+struct Point {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "X={},Y={},Z={}", self.x, self.y, self.z)
+    }
+}
+
+fn parse_input_into_points(text: &str) -> Vec<Point> {
+    text.lines()
+        .map(|l| {
+            let tokens: Vec<&str> = l.split(',').collect();
+            Point {
+                x: tokens[0].parse::<i64>().unwrap(),
+                y: tokens[1].parse::<i64>().unwrap(),
+                z: tokens[2].parse::<i64>().unwrap(),
+            }
+        })
+        .collect()
+}
+
+fn decode_mistery_1(points: &Vec<Point>) -> usize {
     0
 }
 
-fn decode_mistery_2(text: &str) -> usize {
+fn decode_mistery_2(points: &Vec<Point>) -> usize {
     0
 }
 
 fn main() {
     // Parse input
     let text = quick_read!("input.txt");
+    let points = parse_input_into_points(&text);
+    points.iter().for_each(|p| println!("{p}"));
 
     // Return solutions
-    let password_1 = decode_mistery_1(&text);
+    let password_1 = decode_mistery_1(&points);
     println!("Part 1: {password_1}");
 
-    let password_2 = decode_mistery_2(&text);
+    let password_2 = decode_mistery_2(&points);
     println!("Part 2: {password_2}");
 }
